f9ef8c6f14eae7200ac9ab1ac76229cd76e2dd3e
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2019-03-02 17:09:00 +0100

    Test.

--- index.js
+++ index.js
@@ -6,18 +6,13 @@ const util = require('util')
 
 const main = async (message) => {
   const mail = fs.readFileSync(path.join(__dirname, 'templates', 'main', 'index.mustache'), 'utf-8')
+  const logo = path.join(__dirname, 'templates', 'main', 'images', 'itisFermi_white.svg')
+  const css = path.join(__dirname, 'templates', 'main', 'css', 'main.css')
+
   const html = mustache.render(mail, {
     message,
-    logo: path.format({
-      root: __dirname,
-      dir: '/templates/main/images/',
-      base: 'itisFermi_white.svg'
-    }),
-    css: path.format({
-      root: __dirname,
-      dir: '/templates/main/css/',
-      base: 'main.css'
-    })
+    logo,
+    css
   })
   const compile = util.promisify(juice.juiceResources)
   return compile(html, undefined)
